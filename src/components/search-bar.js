ffb8ca795da1477f56200601a272d30e85bd4fb9
DCurrie07 <demarcus.currie@yahoo.com>
2021-09-10 18:58:35 -0500

    created recent posts and search bar components and used them in the app.js component

---
+++ src/components/search-bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <form>
+                <input placeholder="Search DailSmarty" />
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
